18af86d9a47e7c59ac430f1ac6f5080d41b09dfc
kotasai98 <seshasaikota20@gmail.com>
2023-02-28 00:25:52 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=10;
+        int b=100;
+        if(a>b) {
+            System.out.println("A is larger");
+        }else{
+            System.out.println("B is larger");
+        }
+    }
+}
