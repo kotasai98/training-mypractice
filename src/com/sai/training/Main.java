0b6f104d77151e70396d2e5d4d0065fb953cae88
kotasai98 <seshasaikota20@gmail.com>
2023-03-01 22:07:44 -0500

    scanner is added

--- src/com/sai/training/Main.java
+++ src/com/sai/training/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a=10;
-        int b=10;
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("int a:" );
+        a = sc.nextInt();
+        System.out.println("int b:");
+        b= sc.nextInt();
+
         if(a>b) {
             System.out.println("A is larger");
         }else if(b>a) {
